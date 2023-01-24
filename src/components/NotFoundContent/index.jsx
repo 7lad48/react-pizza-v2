907707d700f7b36191a6e02b5040231e8bfb4e81
Your Name <2335213@gmail.com>
2023-01-24 20:12:58 +0300

    notfound page, scss module

--- src/components/NotFoundContent/index.jsx
+++ src/components/NotFoundContent/index.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import styles from './NotFoundContent.module.scss';
-
+import emptyImage from '../../assets/img/pizza-logo.svg';
 const NotFoundContent = () => {
-    
-    return (
-        <div className={styles.content}>
-            <h2 className={styles.title}>Корзина пустая <icon className={styles.icon}>😕</icon></h2>
-            <p>Вероятней всего, вы не заказывали ещё пиццу.<br />
-            Для того, чтобы заказать пиццу, перейди на главную страницу.</p>
-            <img src="" alt="Empty cart" />
-        </div>
-    )
+
+    return (<div className={styles.content}>
+        <h2 className={styles.title}>Страница не найдена <icon className={styles.icon}>😕</icon></h2>
+        <p className={styles.description}>Такой страницы не существует на этом сайте.</p>
+        <img src={emptyImage} alt="Empty cart"/>
+    </div>)
 }
 
 export default NotFoundContent;
-
-<div class="container container--cart">
-                <div class="cart cart--empty">
-                    <h2>Корзина пустая <icon>😕</icon></h2>
-                    
-                    <a href="/" class="button button--black">
-                    <span>Вернуться назад</span>
-                    </a>
-                </div>
-            </div>
